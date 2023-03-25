6b25557201f8cbbece34df6d4d06352a952e3be5
Zhanghao Wu <zhanghao.wu@outlook.com>
2023-03-24 21:19:46 -0700

    update

--- assets/html/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        const center = document.querySelector('.petal5');
-        center.style.opacity = 1;
-    }, 3500);
-});
